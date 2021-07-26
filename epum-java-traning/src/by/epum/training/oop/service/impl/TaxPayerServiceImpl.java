2b9d6c178b8b41be33d4fde8b1fde454f02140c6
Maryna Bastava <nik-vodo@yandex.ru>
2021-07-26 17:09:55 +0300

    add tax payer service implementation

---
+++ epum-java-traning/src/by/epum/training/oop/service/impl/TaxPayerServiceImpl.java
@@ -0,0 +1,42 @@
+package by.epum.training.oop.service.impl;
+
+import java.util.List;
+
+import by.epum.training.oop.dao.DAOProvider;
+import by.epum.training.oop.dao.exception.DAOException;
+import by.epum.training.oop.entity.TaxPayer;
+import by.epum.training.oop.service.TaxPayerService;
+import by.epum.training.oop.service.exception.ServiceException;
+
+
+
+public class TaxPayerServiceImpl implements TaxPayerService{
+	DAOProvider provider=DAOProvider.getInstance();
+	//TaxPayerDAO taxPayerDAO=provider.getTaxPayerDAO();  //what is better
+	
+	@Override
+	public TaxPayer getById(Long id) throws ServiceException {
+		
+		TaxPayer taxPayer=null;
+		try {
+			taxPayer=provider.getTaxPayerDAO().getById(id);
+		}catch(DAOException e) {
+			throw new ServiceException(e);
+		}
+	
+		return taxPayer;
+	}
+
+	@Override
+	public List<TaxPayer> findByLastName(String lastName) throws ServiceException {
+		
+		List <TaxPayer> taxPayers=null;
+		try {
+			taxPayers=provider.getTaxPayerDAO().findByLastName(lastName);
+		}catch(DAOException e) {
+			throw new ServiceException(e);
+		}
+	
+		return taxPayers;
+	}	
+}
