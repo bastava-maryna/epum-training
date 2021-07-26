48c1dcaf2aee6d445699ed29c18f63d1a4e83204
Maryna Bastava <nik-vodo@yandex.ru>
2021-07-26 16:59:56 +0300

    update

--- epum-java-traning/src/by/epum/training/oop/controller/Controller.java
+++ epum-java-traning/src/by/epum/training/oop/controller/Controller.java
@@ -2,17 +2,24 @@ package by.epum.training.oop.controller;
 
 import by.epum.training.oop.command.Command;
 import by.epum.training.oop.command.CommandProvider;
-import by.epum.training.oop.service.ServiceException;
+import by.epum.training.oop.service.exception.ServiceException;
+
 
 public class Controller {
-	
+	//???or private
 	public final CommandProvider provider=new CommandProvider();
 	
 	public String doAction (String request) throws ServiceException {
 		//"FindByID id='10060' 
+		//"FIND_BY_LAST_NAME lastName=Ivanova"
 		//"CalculateAnnualTax id="10060" year="2020"
 		//"GetIncomes id="10060" year="2020"
 		//"GetTaxData year="2020"
+		//"DisplayAnnualTax id=10060 year=2020"
+		//SortAnnualTaxByTax id=10060 year=2020"
+		//"GET_MONTH_INCOMES id=10060 year=2020 month=5"  
+    	//"DISPLAY_Period_TAX id=10060 year=2020 month=5"
+		//"QUIT"
 		
 		String commandName;
 		commandName=request.split("\\s+")[0];
