1bae387cfa345841343379e065e8c5729a940187
Maryna Bastava <nik-vodo@yandex.ru>
2021-08-19 22:45:12 +0300

    add stax readers with cursor and iterator approach

---
+++ epum-java-training-xml/src/by/epum/training/xml/xml_processing/OldPostcardsXmlStaxCursorReader.java
@@ -0,0 +1,163 @@
+package by.epum.training.xml.xml_processing;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
+
+import org.apache.log4j.Logger;
+
+import by.epum.training.xml.domain.OldPostcard;
+import by.epum.training.xml.domain.PostcardTheme;
+import by.epum.training.xml.domain.PostcardType;
+import by.epum.training.xml.domain.ValueType;
+
+public class OldPostcardsXmlStaxCursorReader {
+	private static final Logger LOGGER=Logger.getLogger(OldPostcardsXmlStaxCursorReader.class);
+
+	public static List<OldPostcard> parseXML(String fileName) throws FileNotFoundException, XMLStreamException{
+		List<OldPostcard> postcards = new ArrayList<>();
+		OldPostcard op = null;		
+
+		try {
+			XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+			XMLStreamReader reader = inputFactory.createXMLStreamReader(new FileInputStream(fileName));
+			
+			/**
+			 * Returns an integer code for this event.
+			 * 0-START_ELEMENT
+			 * 1-END_ELEMENT
+			 * 2-CHARACTERS
+			 * 3-ATTRIBUTE
+			 * 4-NAMESPACE
+			 * 5-PROCESSING_INSTRUCTION
+			 * 6-COMMENT
+			 * 7-START_DOCUMENT
+			 * 8-END_DOCUMENT
+			 * 9-DTD
+			 */
+	
+			while (reader.hasNext()) {
+	
+				int eventType = reader.next();
+				
+				System.out.println("eventType="+eventType);
+				if (eventType == XMLEvent.START_ELEMENT) {	
+					
+					switch (reader.getLocalName()) {
+						case "oldPostcard":
+							op = new OldPostcard();
+							String id = reader.getAttributeValue(null,"id");
+							op.setId(Long.parseLong(id));
+							
+							LOGGER.info("\nID "+id);
+							//System.out.println("ID "+id);
+							break;
+							
+						case "description":
+							 eventType = reader.next();
+							 op.setDescription(reader.getText());
+		                    
+							 LOGGER.info("\nDESCRIPTION: "+reader.getText());
+		                     //System.out.println("DESCRIPTION: "+reader.getText());
+		                 
+							break;
+							
+						case "theme":
+							eventType = reader.next();
+							op.setTheme(PostcardTheme.valueOf(reader.getText()));
+							
+							LOGGER.info("\nTHEME: "+reader.getText());
+							//System.out.println("THEME: "+reader.getText());
+							
+							break;
+							
+						case "type":
+							eventType = reader.next();
+							op.setType(PostcardType.valueOf( reader.getText()));
+							
+							LOGGER.info("\nTYPE: "+reader.getText());
+							//
+							System.out.println("TYPE: "+reader.getText());
+							break;
+						case "isSent":
+							eventType = reader.next();
+							op.setSent(Boolean.getBoolean(reader.getText()));
+							
+							LOGGER.info("\nISSENT: "+reader.getText());
+							//System.out.println("ISSENT: "+reader.getText());
+							break;
+							
+						case "country":
+							eventType = reader.next();
+							op.setCountry(reader.getText());
+							
+							LOGGER.info("\nCOUNTRY: "+reader.getText());
+							//System.out.println("COUNTRY: "+reader.getText());
+							break;
+							
+						case "year":
+							eventType = reader.next();
+							op.setYear(Integer.parseInt(reader.getText()));
+							
+							LOGGER.info("\nYEAR: "+reader.getText());
+							//System.out.println("YEAR: "+reader.getText());
+							break;
+							
+						case "authors":
+							eventType = reader.next();
+							List<String> authors=new ArrayList<>();
+							op.setAuthors(authors);
+							
+							LOGGER.info("AUTHORS empty list created");
+							break;
+							
+						case "value":
+							eventType = reader.next();
+							op.setValue(ValueType.valueOf( reader.getText()));
+							
+							LOGGER.info("\nVALUE: "+reader.getText());
+							//System.out.println("VALUE: "+reader.getText());
+							break;	
+							
+						case "author":
+							eventType = reader.next();
+							op.getAuthors().add(reader.getText());
+							
+							LOGGER.info("\nAUTHOR: "+reader.getText());
+							break;	
+						}
+				}
+				if (eventType == XMLEvent.END_ELEMENT) {
+				
+					if (reader.getLocalName().equals("oldPostcard")) {	
+						
+						if(op.getAuthors()==null) {
+							op.setAuthors(new ArrayList<>());
+						}
+						
+						postcards.add(op);
+						LOGGER.info("\nOldPostcard object created:\n"+op.toString());
+					}
+				}
+			}
+		} catch (FileNotFoundException exc) {
+			LOGGER.error(exc);
+			throw new FileNotFoundException(exc.getMessage());
+		} catch (XMLStreamException ex) {
+			LOGGER.error(ex);
+			throw new XMLStreamException(ex.getMessage());
+		}catch(FactoryConfigurationError e) {
+			LOGGER.error(e);
+			throw new FactoryConfigurationError(e.getMessage());
+		}
+		
+		return postcards;
+	}
+}
---
+++ epum-java-training-xml/src/by/epum/training/xml/xml_processing/OldPostcardsXmlStaxIteratorReader.java
@@ -0,0 +1,168 @@
+package by.epum.training.xml.xml_processing;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+import org.apache.log4j.Logger;
+
+import by.epum.training.xml.domain.OldPostcard;
+import by.epum.training.xml.domain.PostcardTheme;
+import by.epum.training.xml.domain.PostcardType;
+import by.epum.training.xml.domain.ValueType;
+
+
+public class OldPostcardsXmlStaxIteratorReader {
+	private static final Logger LOGGER=Logger.getLogger(OldPostcardsXmlStaxIteratorReader.class);
+
+	public static List<OldPostcard> parseXML(String fileName) throws XMLStreamException, FileNotFoundException {
+
+		List<OldPostcard> postcards = new ArrayList<>();
+		OldPostcard op = null;	
+		
+		try {
+			XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+
+			//create EventReader and pass xml file to it
+			XMLEventReader reader = xmlInputFactory.createXMLEventReader(new FileInputStream(fileName));
+
+			//move along all elements(events) 
+			while (reader.hasNext()) {
+				//get event if exist
+				XMLEvent event = reader.nextEvent();
+
+				// stax api iterator EventTypes:
+				//StartDocument         Namespace
+				//StartElement			EntutyReference
+				//EndElement			ProcessingInstruction
+				//Characters			DTD
+				//EndDocument			Comment
+				//Attribute      
+				if (event.isStartElement()) {
+					StartElement startElement = event.asStartElement();
+
+					switch (startElement.getName().getLocalPart()) {
+						case "oldPostcard":
+							op = new OldPostcard();
+							Attribute id = startElement.getAttributeByName(new QName("id"));
+							op.setId(Long.parseLong(id.getValue()));
+							
+							LOGGER.info("\nID "+id.getValue());
+							//System.out.println("ID "+id.getValue());
+							break;
+
+						case "description":
+							event = reader.nextEvent();
+							op.setDescription(event.asCharacters().getData());
+							
+							LOGGER.info("\nDESCRIPTION: "+event.asCharacters().getData());
+							//System.out.println("DESCRIPTION: "+event.asCharacters().getData());
+							break;
+
+						case "theme":	
+							event = reader.nextEvent();
+							op.setTheme(PostcardTheme.valueOf( event.asCharacters().getData()));
+							
+							LOGGER.info("\nTHEME: "+event.asCharacters().getData());
+							//System.out.println("THEME: "+event.asCharacters().getData());
+							break;
+
+						case "type":
+							event = reader.nextEvent();
+							op.setType(PostcardType.valueOf( event.asCharacters().getData()));
+							
+							LOGGER.info("\nTYPE: "+event.asCharacters().getData());
+							//System.out.println("TYPE: "+event.asCharacters().getData());
+							break;
+
+						case "isSent":	
+							event = reader.nextEvent();
+							op.setSent(Boolean.getBoolean(event.asCharacters().getData()));
+							
+							LOGGER.info("\nISSENT: "+event.asCharacters().getData());
+							//System.out.println("ISSENT: "+event.asCharacters().getData());
+							break;
+	
+						case "country":	
+							event = reader.nextEvent();
+							op.setCountry(event.asCharacters().getData());
+							
+							LOGGER.info("\nCOUNTRY: "+event.asCharacters().getData());
+							//System.out.println("COUNTRY: "+event.asCharacters().getData());
+							break;
+
+						case "year":
+							event = reader.nextEvent();
+							op.setYear(Integer.parseInt(event.asCharacters().getData()));
+							
+							LOGGER.info("\nYEAR: "+event.asCharacters().getData());
+							//System.out.println("YEAR: "+event.asCharacters().getData());
+							break;
+	
+						case "authors":
+							event=reader.nextEvent();
+							List<String> authors=new ArrayList<>();
+							op.setAuthors(authors);
+							
+							LOGGER.info("AUTHORS empty list created");
+							break;
+	
+						case "value":	
+							event = reader.nextEvent();
+							op.setValue(ValueType.valueOf( event.asCharacters().getData()));
+							
+							LOGGER.info("\nVALUE: "+event.asCharacters().getData());
+							//System.out.println("VALUE: "+event.asCharacters().getData());
+							break;
+
+						case "author":
+							event=reader.nextEvent();
+							op.getAuthors().add(event.asCharacters().getData());
+							
+							LOGGER.info("\nAUTHOR: "+event.asCharacters().getData());
+							break;
+					}
+				}
+
+				//when reach OldPostCard EndElement , add this card to the list
+				if (event.isEndElement()) {
+					EndElement endElement = event.asEndElement();
+					
+					if (endElement.getName().getLocalPart().equals("oldPostcard")) {
+						
+						if(op.getAuthors()==null) {
+							op.setAuthors(new ArrayList<>());
+						}
+						
+						postcards.add(op);
+						LOGGER.info("\nOldPostcard object created:\n"+op.toString());
+					}
+				}
+			}
+		} catch (FileNotFoundException exc) {
+			LOGGER.error(exc);
+			throw new FileNotFoundException(exc.getMessage());
+		} catch (XMLStreamException ex) {
+			LOGGER.error(ex);
+			throw new XMLStreamException(ex.getMessage());
+		}catch(FactoryConfigurationError e) {
+			LOGGER.error(e);
+			throw new FactoryConfigurationError(e.getMessage());
+		}
+		
+		return postcards;
+	}
+	
+}
